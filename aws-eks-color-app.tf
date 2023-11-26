8c357c8103b89afdad056580b28156d212ee1025
Javier Ruiz Jimenez <jruizjimenez@gmail.com>
2023-11-26 23:21:49 +0800

    Remove network security

--- aws-eks-color-app.tf
+++ aws-eks-color-app.tf
@@ -89,32 +89,6 @@ resource "aws_main_route_table_association" "ditwl-rta-default" {
   route_table_id = aws_route_table.ditwl-rt-pub-main.id
 }
 
-# Create a Security Group
-resource "aws_security_group" "ditwl-sg-eks-01-color-app" {
-  name        = "ditwl-sg-eks-01-color-app"
-  vpc_id      = aws_vpc.ditlw-vpc.id
-}
-
-# Allow access from the Internet to port 8008
-resource "aws_security_group_rule" "ditwl-sr-internet-to-eks-01-color-app-8080" {
-  security_group_id        = aws_security_group.ditwl-sg-eks-01-color-app.id
-  type                     = "ingress"
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  cidr_blocks              = ["0.0.0.0/0"] # Internet
-}
-
-# Allow all outbound traffic to Internet
-resource "aws_security_group_rule" "ditwl-sr-all-outbund" {
-  security_group_id = aws_security_group.ditwl-sg-eks-01-color-app.id
-  type              = "egress"
-  from_port         = "0"
-  to_port           = "0"
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
 # EKS Cluster requires an IAM Role that allows managing AWS resources
 data "aws_iam_policy_document" "ditwl-ipd-eks-01" {
   statement {
